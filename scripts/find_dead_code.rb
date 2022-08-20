d5f265eb4f3068a3651451277c3f030d34a650ac
Alex Bush <alex.v.bush@gmail.com>
2022-08-20 11:27:56 -0500

    update script

--- scripts/find_dead_code.rb
+++ scripts/find_dead_code.rb
@@ -8,8 +8,8 @@ result_stripped_of_absolute_path_prefix = result.gsub(Dir.pwd, '')
 filtered_out_result = result_stripped_of_absolute_path_prefix.split("\n").filter { |line| /:\d+:\d+:/.match?(line) }
 sorted_result = filtered_out_result.sort
 result_with_removed_code_line_number = sorted_result.map {|l| l.sub(/:\d+:\d+:/, '') }
-output = result_with_removed_code_line_number.join("\n")
+output = result_with_removed_code_line_number.join("\n") + "\n"
 
-File.write('periphery_output', output.split("\n").sort.join("\n"))
+File.write('periphery_output', output)
 
 puts "done with dead code analysis"
