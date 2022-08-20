66ec4e1ec7c1ab1000487a3bd31f67e061dda7f1
Alex Bush <alex.v.bush@gmail.com>
2022-08-20 09:42:20 -0500

    Initial Commit

---
+++ DeadCodeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeadCodeProject
+//
+//  Created by Alex Bush on 8/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
